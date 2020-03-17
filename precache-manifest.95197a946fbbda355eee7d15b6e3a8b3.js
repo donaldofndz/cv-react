deed256651a63886b18895e6079cdfd1509a211c
José Donaldo Fernández Montenegro <donaldo@MacBook-Pro-de-Jose.local>
2020-03-16 21:06:10 -0600

    Updates

---
+++ precache-manifest.95197a946fbbda355eee7d15b6e3a8b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d787709677ee60f4fe7f78bb76595ec8",
+    "url": "/cv-react/index.html"
+  },
+  {
+    "revision": "d07a9c5e70e5e2a9b41b",
+    "url": "/cv-react/static/css/main.ee41b223.chunk.css"
+  },
+  {
+    "revision": "843da5fbbd3eb3a71649",
+    "url": "/cv-react/static/js/2.4eb5c35b.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/cv-react/static/js/2.4eb5c35b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d07a9c5e70e5e2a9b41b",
+    "url": "/cv-react/static/js/main.20fb02b7.chunk.js"
+  },
+  {
+    "revision": "88d7288e9779bafada40",
+    "url": "/cv-react/static/js/runtime-main.1418e71b.js"
+  }
+]);
